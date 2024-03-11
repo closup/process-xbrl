16403a64b29551c4527ee57dd539ce72b8b9ecf0
ssuresh17 <160567052+ssuresh17@users.noreply.github.com>
2024-03-11 13:38:04 +0530

    Add files via upload

--- static/js/spinner.js
+++ static/js/spinner.js
@@ -1,11 +1,24 @@
 function updateButton(value) {
     var fileName = value.split('\\').pop();
+    
     if (fileName) {
         document.getElementById('convert-button').style.display = 'block';
         document.getElementById('convert-button').value = 'Convert ' + fileName;
     }
+    
+}
+function updateDocsButton(files,labelId) {
+    document.getElementById(labelId).style.backgroundColor = 'green'
+    var newFileName = "";
+    for (var i = 0; i < files.length; i++) {
+        newFileName += files[i].name + ", ";
+    }
+    newFileName = newFileName.slice(0, -2); 
+    
 }
 
+// Call this function whenever you want to set the previous file name
 function startProcessing() {
-  document.getElementById('loader').style.display = 'block';
+    document.getElementById('loader').style.display = 'block';
+    
 }
