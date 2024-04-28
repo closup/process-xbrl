a3b00fe7ca8c15e95baa6689ae1214d673752cfe
Luca Kato <lucakato@umich.edu>
2024-04-28 17:10:49 -0400

    session works i think? except now struggling to upload 2 or more files

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from app.utils import *
 # flask dependencies
 from flask import Blueprint, request, render_template, jsonify, session
 
-import uiud
+import uuid
 
 # =============================================================
 # Set up routes
@@ -50,6 +50,7 @@ def upload_file():
 
     # Retrieve the list of files from the request
     file_list = request.files.getlist('files[]')
+    print('filelist', file_list)
 
     # Process each file and store filenames
     filenames = []
@@ -59,20 +60,38 @@ def upload_file():
             return jsonify({'error': 'No selected file'}), 400
         if not allowed_file(file.filename):
             return jsonify({'error': 'One or more of your files has a disallowed extension. Allowed extensions are: ' + ", ".join([str(i) for i in ALLOWED_EXTENSIONS])})
-        if file and allowed_file(file.filename):  # allowed files are excel and docx
-            filenames.append(file.filename)
-        if is_spreadsheet(file.filename):
-            excel_files += [file]
-    # confirm that there is exactly one Excel file among the uploads
+        
+        # Save uploaded files to session input folder
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(input_folder, file.filename))
+    
+    # Process uploaded files and save output to session output folder
+    excel_files = [file for file in file_list if is_spreadsheet(file.filename)]
+    print('test', excel_files)
     if len(excel_files) != 1:
         return jsonify({'error': 'Please upload exactly one Excel file'})
+    
+    # define output path
+    output_file = os.path.join(output_folder, 'output.html')
+
     # create ixbrl file
     print("Converting Excel to inline XBRL...")
     write_html(file_list, output_file, format)
     # create page for the interactive viewer (prints progress in create_viewer_html fn)
     viewer_file_name = "app/templates/site/viewer.html"
     create_viewer_html(output_file, viewer_file_name)
+
     return jsonify({'message': 'Files successfully uploaded'})
+
+
+@routes_bp.route('/check_session')
+def check_session():
+    if 'session_id' in session:
+        session_id = session['session_id']
+        return f"Session ID: {session_id}"
+    else:
+        return "Session not found"
+
 
 @routes_bp.route('/upload/complete', methods=['GET'])
 def successful_upload():
