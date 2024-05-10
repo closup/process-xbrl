114e8cb60011b373a0c6fc658094fe07a2b3e6a7
Luca Kato <lucakato@umich.edu>
2024-05-10 17:13:20 -0400

    downloading xbrl as html test

--- app/static/js/download.js
+++ app/static/js/download.js
@@ -2,14 +2,10 @@
 
 function changeDownloadLocation() {
     var sessionID = "{{ session.get('session_id') }}";
-    window.alert(sessionID);
+    console.log(sessionID);
 
-    // Get the current URL of the file to download
-    var currentURL = "{{ url_for('static', filename='output/output.html') }}";
-
-    // Define the new download location with session ID
-    var newURL = currentURL.replace("output/output.html", sessionID + "/output/output.html");
-
+    var newURL = "/static/sessions_data/" + sessionID + "/output/output.html";
+    
     // Set the new download location
     document.getElementById("downloadLink").setAttribute("href", newURL);
 }
