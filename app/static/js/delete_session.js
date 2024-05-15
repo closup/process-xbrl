67c2dafde0ecace751f2cf174bedbd582644c25a
Luca Kato <lucakato@umich.edu>
2024-05-15 09:27:32 -0400

    moved preventDefault code

--- app/static/js/delete_session.js
+++ app/static/js/delete_session.js
@@ -1,8 +1,9 @@
 window.addEventListener("beforeunload", function(event) {
+    event.preventDefault()
+
     // Send an AJAX request to delete the session folder
     var sessionID = window.sessionID; // Get the session ID
     console.log('js session id', sessionID);
-    event.preventDefault()
     var url = "/delete_session?session_id=" + encodeURIComponent(sessionID);
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url);
