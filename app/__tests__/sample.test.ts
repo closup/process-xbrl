0b3b1b4615d600b41d90fb07d41af395aaabaf6f
Luca Kato <lucakato@umich.edu>
2024-12-16 00:27:35 -0500

    the tests seem to work pretty well!

--- app/__tests__/sample.test.ts
+++ app/__tests__/sample.test.ts
@@ -1,9 +1,13 @@
 import { test } from '@antiwork/shortest'
 
+// Specify file to test. City of Brea, CA
+test('Click `Upload file(s)` button. Upload City of Brea, CA.xlsx located in `app/models/static/input_files/samples`')
 
 // Test conversion new page
-test('Verify new page loads opens after user clicks `Combine and convert file(s)` button. New page has 3 buttons, `Download file`, `Click to view in browser`, and `Convert Another ACFR`.')
+test('Once file is uploaded, verify new page loads opens after user clicks `Combine and convert file(s)` button. New page has 3 buttons, `Download file`, `Click to view in browser`, and `Convert Another ACFR`.')
 
-// Test notification messages
-test('Verify notification messages are displayed correctly in top-left corner of screen during the conversion process, following user click of `Combine and convert file(s)` button. \
-    Message displayed should be the following: `Initializing upload`, `Uploading files`, `Processing and validating files`, `Creating iXBRL file`, `Creating viewer`')
+// Test iXBRL viewer and new tab
+test('Once conversion is complete and new page loads, verify iXBRL viewer works by clicking on `Click to view in browser` button. Verify it opens a new tab and loads the iXBRL viewer.')
+
+// Test download file
+test('Once conversion is complete and new page loads, verify `Download file` button works by clicking on it. Verify it downloads the file.')
