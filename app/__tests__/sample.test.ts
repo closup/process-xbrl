b760d8abe942578cfe846b27a9b43b5fa479737c
Luca Kato <lucakato@umich.edu>
2024-12-16 00:06:49 -0500

    Sample test

--- app/__tests__/sample.test.ts
+++ app/__tests__/sample.test.ts
@@ -1,1 +1,9 @@
- 
+import { test } from '@antiwork/shortest'
+
+
+// Test conversion new page
+test('Verify new page loads opens after user clicks `Combine and convert file(s)` button. New page has 3 buttons, `Download file`, `Click to view in browser`, and `Convert Another ACFR`.')
+
+// Test notification messages
+test('Verify notification messages are displayed correctly in top-left corner of screen during the conversion process, following user click of `Combine and convert file(s)` button. \
+    Message displayed should be the following: `Initializing upload`, `Uploading files`, `Processing and validating files`, `Creating iXBRL file`, `Creating viewer`')
